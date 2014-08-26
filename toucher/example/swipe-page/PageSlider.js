1ab505dd9b5363106b837dd385bee3573b73a050
finscn <finscn@gmail.com>
2014-08-26 12:42:01 +0800

    update

--- toucher/example/swipe-page/PageSlider.js
+++ toucher/example/swipe-page/PageSlider.js
@@ -57,16 +57,17 @@ PageSlider.prototype = {
         dom.style.webkitTransform = "translate3d(" + x + "," + y + ",0)";
         dom.style.transform = "translate3d(" + x + "," + y + ",0)";
     },
-    gotoPage: function(idx){
+    gotoPage: function(idx,end){
         if (idx<0){
-            this.gotoPage(0);
+            this.gotoPage(0,true);
         }else if (idx>this.pageCount-1){ 
-            this.gotoPage(this.pageCount-1);
+            this.gotoPage(this.pageCount-1,true);
         }else{
             this.currentPage = idx;
             var p = this.pageList[idx];
             this.y=-idx * this.height;
-            info.innerHTML=this.y;
+            this.pageContainer.classList.remove(end?"page-tween":"end-tween");
+            this.pageContainer.classList.add(end?"end-tween":"page-tween");
             this.setDomPos(this.pageContainer, 0, this.y + "px");
         }
     },
@@ -75,28 +76,36 @@ PageSlider.prototype = {
             this.currentPage++;
             var p = this.pageList[this.currentPage];
             this.y=-this.currentPage * this.height;
+            this.pageContainer.classList.add("page-tween");
             this.setDomPos(this.pageContainer, 0, this.y + "px");
         } else {
             this.y=this.minY;
             var Me = this;
+            this.pageContainer.classList.add("page-tween");
             this.setDomPos(Me.pageContainer, 0, Me.minY-100+"px");
             setTimeout(function() {
+                Me.pageContainer.classList.remove("page-tween");
+                Me.pageContainer.classList.add("end-tween");
                 Me.setDomPos(Me.pageContainer, 0, Me.minY+"px");
-            }, 250);
+            }, 300);
         }
     },
     prevPage: function() {
         if (this.currentPage > 0) {
             this.currentPage--;
             this.y=-this.currentPage * this.height;
+            this.pageContainer.classList.add("page-tween");
             this.setDomPos(this.pageContainer, 0, this.y+"px");
         } else {
             this.y=this.maxY;
             var Me = this;
+            this.pageContainer.classList.add("page-tween");
             this.setDomPos(Me.pageContainer, 0, Me.maxY+100+"px");
             setTimeout(function() {
+                Me.pageContainer.classList.remove("page-tween");
+                Me.pageContainer.classList.add("end-tween");
                 Me.setDomPos(Me.pageContainer, 0, Me.maxY+"px");
-            }, 250);
+            }, 300);
         }
     }
 }
