f9f4018989292e1fbf71c2c8c813672efc2c063b
Rocky Wei <finscn@gmail.com>
2017-11-02 18:34:09 +0800

    update

--- million-bunnies/config.js
+++ million-bunnies/config.js
@@ -2,6 +2,6 @@ var width = 900;
 var height = 600;
 // var fboSize = 1024 * 2;
 var fboSize = 200;
-var zoom = 1.0;
+var zoom = 2.0;
 var bunniesToRender = 1000000 // 1048576 ;
 var bunniesToRender = 16384;
