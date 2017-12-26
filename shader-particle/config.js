61bad8b7ed6087f03248381981b636c935987c3c
finscn <finscn@gmail.com>
2017-12-27 05:21:42 +0800

    update

--- shader-particle/config.js
+++ shader-particle/config.js
@@ -7,4 +7,4 @@ var particleCount = parseInt(params.p) || 2 * 10000;
 var zoom = parseFloat(params.z) || 0.5 * 2;
 
 var useFilter = parseFloat(params.f) || false;
-var useFramebuffer = parseFloat(params.fb) || false;
+var useFramebuffer = parseFloat(params.fb) || null;
