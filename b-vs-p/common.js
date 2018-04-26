e59dc11da121216f2887bf303af985af4c6d71c8
finscn <finscn@gmail.com>
2018-04-27 05:20:00 +0800

    add b-vs-p demo

---
+++ b-vs-p/common.js
@@ -0,0 +1,39 @@
+var canvas, context;
+var world;
+
+var WIDTH = 800;
+var HEIGHT = 500;
+var COUNT = 500;
+var boxSize = 0.4;
+
+var stats = new Stats();
+
+function randomInt(min, max) {
+    return ((max - min + 1) * Math.random() + min) >> 0;
+}
+
+function drawPoly(ctx, vertices, vertexCount) {
+    ctx.beginPath();
+    ctx.moveTo(vertices[0].x, vertices[0].y);
+    for (var i = 1; i < vertexCount; i++) {
+        ctx.lineTo(vertices[i].x, vertices[i].y);
+    }
+    ctx.closePath();
+    ctx.strokeStyle = '#ffffff';
+    ctx.stroke();
+}
+
+
+window.onload = function() {
+    canvas = document.getElementById('stage');
+    canvas.width = WIDTH;
+    canvas.height = HEIGHT;
+
+    if (stats.domElement) {
+        stats.domElement.style.cssText = 'opacity:0.6; position:absolute; right:0px; top:0px; z-index:9999;';
+        document.body.appendChild(stats.domElement);
+    }
+
+    context = canvas.getContext('2d');
+    animate();
+}
